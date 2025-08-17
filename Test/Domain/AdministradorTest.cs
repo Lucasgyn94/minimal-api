7e9546a3cc133f98a4a89ccf4d19fa99abd54021
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-08-17 15:57:26 -0300

    Criando teste de unidade para o modelo de administrador e de veículos

---
+++ Test/Domain/AdministradorTest.cs
@@ -0,0 +1,29 @@
+﻿using MinimalApi;
+
+namespace Test;
+
+[TestClass]
+public class AdministradorTest
+{
+    
+    [TestMethod]
+    public void TestarGetSetPropriedadesAdministradores()
+    {
+        // Arrange: onde fica todas as variáveis necessárias para as validações
+
+        var adm = new Administrador();
+
+        // Act: ação que vamos realizar em cima de nossos dados
+        adm.Id = 1;
+        adm.Email = "administrador@teste.com.br";
+        adm.Senha = "123456";
+        adm.Perfil = "Adm";
+
+        // Assert: validação dos dados
+        Assert.AreEqual(1, adm.Id);
+        Assert.AreEqual("administrador@teste.com.br", adm.Email);
+        Assert.AreEqual("123456", adm.Senha);
+        Assert.AreEqual("Adm", adm.Perfil);
+    }
+    
+}
---
+++ Test/Domain/VeiculoTest.cs
@@ -0,0 +1,28 @@
+﻿using MinimalApi;
+
+namespace Test;
+
+[TestClass]
+public class VeiculoTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedadesVeiculos()
+    {
+        // Arrange
+        var veiculo = new Veiculo();
+
+        // Act
+        veiculo.Id = 1;
+        veiculo.Nome = "m200";
+        veiculo.Marca = "bmw";
+        veiculo.Ano = 2025;
+
+        // Assert
+        Assert.AreEqual(1, veiculo.Id);
+        Assert.AreEqual("m200", veiculo.Nome);
+        Assert.AreEqual("bmw", veiculo.Marca);
+        Assert.AreEqual(2025, veiculo.Ano);
+
+    }
+
+}
