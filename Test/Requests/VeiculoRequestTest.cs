5ced988983e92b1d0dfb49efbaddb2a40ef48a35
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-08-19 00:12:04 -0300

    finalização testes de request dos nossos endpoints de veículos

--- Test/Requests/VeiculoRequestTest.cs
+++ Test/Requests/VeiculoRequestTest.cs
@@ -106,11 +106,175 @@ public class VeiculoRequestTest
         // Assert
         Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
     }
+
+    [TestMethod]
+    public async Task CriarVeiculoComDadosInvalidosDeveRetornarBadRequest()
+    {
+        // arrange
+        var token = await LoginAssincronoEPegarToken("editor@teste.com", "123456");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+        var veiculoDto = new VeiculoDTO
+        {
+            Nome = "Fusca",
+            Marca = "VW",
+            Ano = 1920
+        };
+        var content = JsonContent.Create(veiculoDto);
+
+        // act
+        var response = await _client.PostAsync("/veiculos", content);
+
+        // assert
+        Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+
+    }
+    #endregion
+
+    #region Testes de Obter Veículos (GET /veiculos e GET /veiculos/{id})
+
+    [TestMethod]
+    public async Task ObterVeiculoPorIdComIdValidoEAutorizacaoDeveRetornarOk()
+    {
+        // Arrange
+        var token = await LoginAssincronoEPegarToken("editor@teste.com", "123456");
+        var veiculoCriado = await CriarVeiculoParaTesteAsync(token);
+
+        // Act
+        var response = await _client.GetAsync($"/veiculos/{veiculoCriado.Id}");
+
+        // Assert        
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        var veiculoRetornado = await response.Content.ReadFromJsonAsync<Veiculo>(_jsonOptions);
+        Assert.IsNotNull(veiculoRetornado);
+        Assert.AreEqual(veiculoCriado.Nome, veiculoCriado.Nome);
+    }
+
+    [TestMethod]
+    public async Task ObterVeiculoPorIdComIdInvalidoDeveRetornarNotFound()
+    {
+        // Arrange
+        var token = await LoginAssincronoEPegarToken(
+            "editor@teste.com",
+            "123456"
+        );
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+        // Act
+        var response = await _client.GetAsync("/veiculos/999"); // passando id invalido
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    #endregion
+
+    #region Testes de Atualizar Veículo {PUT /veiculos/{id}}
+
+    [TestMethod]
+    public async Task AtualizarVeiculoComDadosValidosEAutorizacaoAdminDeveRetornarOk()
+    {
+        // Arrange
+        var tokenAdmin = await LoginAssincronoEPegarToken(
+            "adm@teste.com",
+            "123456"
+        );
+
+        var veiculoCriado = await CriarVeiculoParaTesteAsync(tokenAdmin);
+
+        var veiculoAtualizadoDto = new VeiculoDTO
+        {
+            Nome = "C-200",
+            Marca = "Mercedez",
+            Ano = 2025
+        };
+        var content = JsonContent.Create(veiculoAtualizadoDto);
+
+        // Act
+        var response = await _client.PutAsync($"/veiculos/{veiculoCriado.Id}", content);
+
+        // Assert        
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        var veiculoRetornado = await response.Content.ReadFromJsonAsync<Veiculo>(_jsonOptions);
+        Assert.IsNotNull(veiculoRetornado);
+        Assert.AreEqual("C-200", veiculoRetornado.Nome);
+    }
+
+    [TestMethod]
+    public async Task AtualizarVeiculoComAutorizacaoEditorDeveRetornarForbidden()
+    {
+        // Arrange
+        var tokenAdmin = await LoginAssincronoEPegarToken(
+            "adm@teste.com",
+            "123456"
+        );
+        var veiculoCriado = await CriarVeiculoParaTesteAsync(tokenAdmin);
+
+        var tokenEditor = await LoginAssincronoEPegarToken(
+            "editor@teste.com",
+            "123456"
+        );
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", tokenEditor);
+
+        var veiculoAtualizadoDto = new VeiculoDTO
+        {
+            Nome = "C-200",
+            Marca = "Mercedez",
+            Ano = 2024
+        };
+        var content = JsonContent.Create(veiculoAtualizadoDto);
+
+        // Act
+        var response = await _client.PutAsync($"/veiculos/{veiculoCriado}", content);
+
+        // Assert        
+        Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+    }
+
+    #endregion
+
+    #region Testes de Apagar Veículo (DELETE / veiculos/{id})
+
+    [TestMethod]
+    public async Task ApagarVeiculoComIdValidoEAutorizacaoAdminDeveRetornarNoContent()
+    {
+        // Arrange
+        var tokenAdmin = await LoginAssincronoEPegarToken(
+            "adm@teste.com",
+            "123456"
+        );
+        var veiculoCriado = await CriarVeiculoParaTesteAsync(tokenAdmin);
+
+        // Act
+        var response = await _client.DeleteAsync($"/veiculos/{veiculoCriado.Id}");
+
+        // Assert 
+        Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);       
+
+    }
+
+    [TestMethod]
+    public async Task ApagarVeiculoComAutorizacaoEditorDeveRetornarForbidden()
+    {
+        // Arrange
+        var tokenAdmin = await LoginAssincronoEPegarToken(
+            "adm@teste.com",
+            "123456"
+        );
+        var veiculoCriado = await CriarVeiculoParaTesteAsync(tokenAdmin);
+
+        var tokenEditor = await LoginAssincronoEPegarToken(
+            "editor@teste.com",
+            "123456"
+        );
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", tokenEditor);
+
+        // Act
+        var response = await _client.DeleteAsync($"veiculos/{veiculoCriado.Id}");
+
+        // Assert
+
+        Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+    }
+
     #endregion
 }
 
-        // Arrange
-
-        // Act
-
-        // Assert
