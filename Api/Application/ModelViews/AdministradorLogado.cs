712756591ef0693424e61e4a86a2c132ad188a8b
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-08-31 16:15:01 -0300

    atualização model views administrador logado

--- Api/Application/ModelViews/AdministradorLogado.cs
+++ Api/Application/ModelViews/AdministradorLogado.cs
@@ -3,6 +3,6 @@
 public record class AdministradorLogado
 {
     public string Email { get; set; } = default!;
-    public String Perfil { get; set; } = default!;
+    public string Perfil { get; set; } = default!;
     public string Token { get; set; } = default!;
 }
